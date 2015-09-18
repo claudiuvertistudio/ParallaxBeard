26369b7f1d7dc2bb74068fe688fe3e9e6647d0b2
unknown <andrei@9pixels.co>
2015-09-18 23:47:37 +0300

    General: parallax footer, culori footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function($) {
 
+    /**
+     * Make footer fixed perfectly
+     */
+	$( ".footer" ).css( "height", Math.floor( $( ".footer_parallax" ).outerHeight( true ) ) );
+   	$(window).on("resize scroll",function(){
+	    if( jQuery( window ).height() <= ( $( ".footer" ).outerHeight( true ) + $( ".sticky-navigation.appear-on-scroll" ).outerHeight( true ) ) || jQuery( window ).width() <= 1200 ){
+	        $(".footer_parallax").removeAttr('style');
+	    }else{
+			var offsetFooter = $( ".footer" ).offset();
+			$( '.footer_parallax' ).css( 'bottom', - ( ( $( this ).scrollTop() + jQuery( window ).height() ) - offsetFooter.top - $( ".footer" ).outerHeight( true ) - 15 ) ); //body border: 15px;
+	    }
+	}).trigger( 'resize scroll' );
+
 	$(window).on('scroll', function() {
 
 		if(!$('.appear-on-scroll').hasClass('scroll')) {
